55dc492c968b56c5145beb4370276d550ae5fc35
Alexandr Shatskyy <autome@ukr.net>
2025-07-04 15:21:33 +0200

    finish home page

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,9 +1,31 @@
-
+import { Link } from "react-router-dom";
 
 export const Footer = () => {
   return (
-		<section className="footer">
-		F O O T E R
+    <section className="footer">
+      <div className="footer-main-container">
+        <Link to="/" className="logo-link">
+          <img className="logo" src="./images/dine.svg" alt="logo" />
+				</Link>
+				
+				<div className="footer-info-container">
+					
+          <div className="footer-address-container">
+            MARTHWAITE, SEDBERGH
+            <br />
+            CUMBRIA
+            <br />
+            +00 44 123 4567
+          </div>
+          <div className="footer-open-times-container">
+            OPEN TIMES
+            <br /> MON - FRI: 09:00 AM - 10:00 PM
+            <br /> SAT - SUN: 09:00 AM - 11:30 PM
+          </div>
+				</div>
+				
+		</div> 
+		
     </section>
   );
 };
